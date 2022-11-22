bbe49d6541d7eb500ef30f37248577541281ca28
hazel-davis <hazel.davis@answerdigital.com>
2022-11-22 14:32:37 +0000

    added copies of prodtests

--- tests/Answer.King.Api.IntegrationTests/Controllers/OrderControllerTests.cs
+++ tests/Answer.King.Api.IntegrationTests/Controllers/OrderControllerTests.cs
@@ -1,7 +1,7 @@
 ﻿using Alba;
 using Answer.King.Api.IntegrationTests.Common;
 using Answer.King.Api.RequestModels;
-using Product = Answer.King.Api.IntegrationTests.Common.Models.Product;
+using Order = Answer.King.Api.IntegrationTests.Common.Models.Order;
 
 namespace Answer.King.Api.IntegrationTests.Controllers;
 
@@ -20,6 +20,7 @@ public class OrderControllerTests : IClassFixture<WebFixtures>
         this._errorLevelSettings.ScrubMember("traceId");
     }
 
+    #region Get
     [Fact]
     public async Task<VerifyResult> GetOrders_ReturnsList()
     {
@@ -32,4 +33,232 @@ public class OrderControllerTests : IClassFixture<WebFixtures>
         var orders = result.ReadAsJson<IEnumerable<Order>>();
         return await Verify(orders);
     }
+
+    [Fact]
+    public async Task<VerifyResult> GetOrder_OrderExists_ReturnsOrder()
+    {
+        var result = await this._host.Scenario(_ =>
+        {
+            _.Get.Url("/api/orders/1");
+            _.StatusCodeShouldBeOk();
+        });
+
+        var orders = result.ReadAsJson<Order>();
+        return await Verify(orders);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> GetOrder_OrderDoesNotExist_Returns404()
+    {
+        var result = await this._host.Scenario(_ =>
+        {
+            _.Get.Url("/api/orders/50");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.NotFound);
+        });
+
+        return await VerifyJson(result.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+    #endregion
+
+    #region Post
+    [Fact]
+    public async Task<VerifyResult> PostOrder_ValidModel_ReturnsNewOrder()
+    {
+        var result = await this._host.Scenario(_ =>
+        {
+            _.Post
+                .Json(new
+                {
+                    Name = "Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl("/api/orders");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.Created);
+        });
+
+        var orders = result.ReadAsJson<Order>();
+        return await Verify(orders);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> PostOrder_InValidDTO_Fails()
+    {
+        var result = await this._host.Scenario(_ =>
+        {
+            _.Post
+                .Json(new
+                {
+                    Name = "Burger",Product
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 4 }
+                })
+                .ToUrl("/api/orders");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.BadRequest);
+        });
+
+        return await VerifyJson(result.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+    #endregion
+
+    #region Put
+    [Fact]
+    public async Task<VerifyResult> PutOrder_ValidDTO_ReturnsModel()
+    {
+        var postResult = await this._host.Scenario(_ =>
+        {
+            _.Post
+                .Json(new
+                {
+                    Name = "Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl("/api/orders");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.Created);
+        });
+
+        var orders = postResult.ReadAsJson<Order>();
+
+        var putResult = await this._host.Scenario(_ =>
+        {
+            _.Put
+                .Json(new
+                {
+                    Name = "BBQ Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl($"/api/orders/{orders?.Id}");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.OK);
+        });
+
+        var updatedOrder = putResult.ReadAsJson<Order>();
+        return await Verify(updatedOrder);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> PutOrder_InvalidDTO_ReturnsBadRequest()
+    {
+        var putResult = await this._host.Scenario(_ =>
+        {
+            _.Put
+                .Json(new
+                {
+                    Name = "BBQ Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 4 }
+                })
+                .ToUrl("/api/orders/1");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.BadRequest);
+        });
+
+        return await VerifyJson(putResult.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> PutOrder_InvalidId_ReturnsNotFound()
+    {
+        var putResult = await this._host.Scenario(_ =>
+        {
+            _.Put
+                .Json(new
+                {
+                    Name = "BBQ Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl("/api/orders/5");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.NotFound);
+        });
+
+        return await VerifyJson(putResult.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+    #endregion
+
+    #region Retire
+    [Fact]
+    public async Task<VerifyResult> RetireOrder_InvalidId_ReturnsNotFound()
+    {
+        var putResult = await this._host.Scenario(_ =>
+        {
+            _.Delete
+                .Url("/api/orders/5");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.NotFound);
+        });
+
+        return await VerifyJson(putResult.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> RetireOrder_ValidId_ReturnsOk()
+    {
+        var postResult = await this._host.Scenario(_ =>
+        {
+            _.Post
+                .Json(new
+                {
+                    Name = "Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl("/api/orders");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.Created);
+        });
+
+        var orders = postResult.ReadAsJson<Order>();
+
+        var putResult = await this._host.Scenario(_ =>
+        {
+            _.Delete
+                .Url($"/api/orders/{orders?.Id}");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.OK);
+        });
+
+        return await VerifyJson(putResult.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+
+    [Fact]
+    public async Task<VerifyResult> RetireOrder_ValidId_IsRetired_ReturnsNotFound()
+    {
+        var postResult = await this._host.Scenario(_ =>
+        {
+            _.Post
+                .Json(new
+                {
+                    Name = "Burger",
+                    Description = "Juicy",
+                    Price = 1.50,
+                    Categories = new List<long> { 1 }
+                })
+                .ToUrl("/api/orders");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.Created);
+        });
+
+        var orders = postResult.ReadAsJson<Order>();
+
+        await this._host.Scenario(_ =>
+        {
+            _.Delete
+                .Url($"/api/orders/{orders?.Id}");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.OK);
+        });
+
+        var secondDeleteResult = await this._host.Scenario(_ =>
+        {
+            _.Delete
+                .Url($"/api/orders/{orders?.Id}");
+            _.StatusCodeShouldBe(System.Net.HttpStatusCode.Gone);
+        });
+
+        return await VerifyJson(secondDeleteResult.ReadAsTextAsync(), this._errorLevelSettings);
+    }
+    #endregion
 }
