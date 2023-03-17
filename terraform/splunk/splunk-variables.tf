7fb54f59cdfdaf558c2506cce7c18638af72f8a3
Calvin St Louis <calvin.stlouis@answerdigital.com>
2023-03-17 16:57:50 +0000

    moved splunk variables to local folder for the splunk terraform code, in order to be visible

---
+++ terraform/splunk/splunk-variables.tf
@@ -0,0 +1,11 @@
+variable "splunk_project_name" {
+    type = string
+    description = "Splunk Project Name"
+    default = "answerking-splunk-instance"
+}
+
+variable "splunk_project_owner" {
+    type = string
+    description = "Splunk Resource Owner"
+    default = "answerking"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -69,15 +69,3 @@ variable "efs_port" {
   description = "EFS Access Port"
   default     = 2049
 }
-
-variable "splunk_project_name" {
-    type = string
-    description = "Splunk Project Name"
-    default = "answerking-splunk-instance"
-}
-
-variable "splunk_project_owner" {
-    type = string
-    description = "Splunk Resource Owner"
-    default = "answerking"
-}
