d2c53e7cd1aecd57babcd35335f79a336faffd22
Calvin St Louis <calvin.stlouis@answerdigital.com>
2023-04-12 19:01:53 +0100

    Added skip for aws autoscaling EC2 launch template checkov check

--- terraform/launch-config.tf
+++ terraform/launch-config.tf
@@ -27,6 +27,7 @@ data "template_file" "user_data" {
 resource "aws_launch_configuration" "ecs_launch_config" {
     #checkov:skip=CKV_AWS_79:TODO: Disable the Instance Metadata Service or enable it with proper configuration (v2)
     #checkov:skip=CKV_AWS_8:TODO: Encrypt volume in future security ticket
+    #checkov:skip=CKV_AWS_315:TODO: Look into aws autoscaling if necessary
     image_id             = data.aws_ami.ecs_ami.id
     iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
     security_groups      = [aws_security_group.ecs_sg.id]
